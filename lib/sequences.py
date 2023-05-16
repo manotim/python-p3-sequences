ad3001061e1ed16c548da1434b55890250df66fb
manoti <manotimike@gmail.com>
2023-05-16 15:27:09 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    empty_list = []
+    a, b = 0, 1
+    while(len(empty_list) < length):
+        empty_list.append(a)
+        a, b = b, a + b
+    print (empty_list)
     pass
